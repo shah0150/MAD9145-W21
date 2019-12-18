7ef255605ef3285d3711d592e14281857fd743f4
Adesh Nilesh Shah <shaha@algonquincollege.com>
2019-12-18 12:17:10 -0500

    general update

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:shah0150/mad9145-w20.git master:gh-pages
+# git push -f git@github.com:shah0150/mad9145-w20.git master:gh-pages
+git push -f https://github.com/shah0150/mad9145-w20.git master:gh-pages
 
 cd -
